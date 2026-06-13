a98028a36f08f80034c815126f08efa0cca5a177
Alex Lin <alexlin8888@gmail.com>
2026-06-13 15:12:07 +0800

    batch 137fix: remove nonexistent geometry.size import

--- app/src/main/java/com/careersandbox/app/ui/screens/interview/InterviewQuickScreen.kt
+++ app/src/main/java/com/careersandbox/app/ui/screens/interview/InterviewQuickScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.size
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
